af641f4833939371992c443b3790c9463fbbee9b
NFalzon1 <nicholas.falzon.d56570@mcast.edu.mt>
2024-03-05 09:33:48 +0100

    Commit 4-3-2024
    Changing of Header

--- header.php
+++ header.php
@@ -17,46 +17,24 @@
 
 
 <body <?php body_class(); ?>> <!--Adds classes to body-->
-  <div class='container-fluid' style='background-color:<?php echo $footer_bg ?>'>
-
-    <div id="theme_header" class="navHeader">
-
-      <div <?php if ($logoPos == "top") {
-        echo "id='logo_image_top'";
-      } else {
-        echo "class='display_none'";
-      } ?>>
-        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_theme_mod('diwp_logo'); ?>" /></a>
-      </div>
-
-      <header class="navbar navbar-expand-lg paddinglfrt">
-        <div class="container-fluid menuButton">
-          <div <?php if ($logoPos == "left") {
-            echo "id='logo_image_left'";
-          } else {
-            echo "class='display_none'";
-          } ?>>
-            <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_theme_mod('diwp_logo'); ?>" /></a>
-          </div>
-          <button class="navbar-toggler " type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <div class="navbar-nav me-auto mb-2 mb-lg-0 main-menu-div w-100">
-              <?php
-              wp_nav_menu(
-                array(
-                  'theme_location' => 'main-menu',
-                )
-              );
-              ?>
+<header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <?php
+                    // Display the WordPress navigation menu
+                    wp_nav_menu(array(
+                        'theme_location' => 'primary-menu',
+                        'container'      => false,
+                        'menu_class'     => 'navbar-nav ml-auto',
+                    ));
+                    ?>
+                </div>
             </div>
-            <?php get_search_form(); ?>
-          </div>
-        </div>
-    </div>
+        </nav>
     </header>
-  </div>
+
